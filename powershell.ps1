555eea001c4ca2cd65c034135b5354b260031a95
rajyalakshmi1203 <bestharajyalakshmi@gmail.com>
2025-12-15 15:36:41 +0530

    Delete powershell.ps1

--- powershell.ps1
+++
@@ -1,8 +0,0 @@
-$use = [PSCustomObject]@{
-    firstname = "rajya"
-    secondname = "lakshmi"
-    age = 30
-    occupation = "housewife"
-}
-$use.firstname
-"Full_Name = $($use.firstname) $($use.secondname)"
